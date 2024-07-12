a2cc58df91864973eb8129d0204d34029fe9d8c1
modem1 <monika.dembek@knowit.eu>
2024-07-12 14:09:44 +0200

    Setup json server, add sample data, add service with basic get  endpoints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskBoardService } from './features/task-board/services/task-board.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'task-board';
+
+  taskBoardService = inject(TaskBoardService);
+
+  ngOnInit(): void {
+    this.taskBoardService
+      .getBoards()
+      .subscribe(data => console.log('boards: ', data));
+
+    this.taskBoardService
+      .getListsForBoard(1)
+      .subscribe(data => console.log('returned lists: ', data));
+
+    this.taskBoardService
+      .getCardsForList(2)
+      .subscribe(data => console.log('returned cards: ', data));
+  }
 }
